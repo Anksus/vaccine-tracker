9f4b26db8b1f08c1c10d9037e754db14da6ff911
Anksus <sankitpramod.min18@itbhu.ac.in>
2021-05-07 23:33:51 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@
 const figlet = require("figlet");
 const chalk = require("chalk");
 const inquirer = require("../lib/inquirer");
-const Axios = require("axios");
-
 
 const run = async () => {
-  const credentials = await inquirer.askStateAndDistrict();
+  figlet("Vaccine Tracker", function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    console.log(chalk.red.bold(data));
+    console.log(
+      chalk.red.bold(
+        "CLI tool to check the Cowin APIs every minute and alert the user when a vaccine is available."
+      )
+    );
+    console.log(chalk.green.bold("Select your state, district and age-group!"));
+  });
+
+  await inquirer.askStateAndDistrict();
 };
 
 run();
