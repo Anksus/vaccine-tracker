9c64eb24e520ac994d7967fccdac26fd6bf37b32
Anksus <sankitpramod.min18@itbhu.ac.in>
2021-05-07 22:50:39 +0530

    stuff working

--- src/index.js
+++ src/index.js
@@ -5,25 +5,9 @@ const chalk = require("chalk");
 const inquirer = require("../lib/inquirer");
 const Axios = require("axios");
 
-// figlet("Covid Vaccine", function (err, data) {
-//   if (err) {
-//     console.log("Something went wrong...");
-//     console.dir(err);
-//     return;
-//   }
-//   console.log(chalk.red(data));
-//   console.log(
-//     chalk.red(
-//       "Command line tool for checking Cowin API's and alerts when vaccine is avialble."
-//     )
-//   );
-// });
 
 const run = async () => {
   const credentials = await inquirer.askStateAndDistrict();
-
-  //   console.log(getStates());
-  //   console.log(credentials);
 };
 
 run();
